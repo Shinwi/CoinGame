bf997180da864c315f4ddd1966224cdf0f2beeae
Oualid Ezz <siszko37@gmail.com>
2021-06-02 10:08:53 +0200

    added the createBoard() method

--- src/main/java/coingame/javafx/controllers/GameController.java
+++ src/main/java/coingame/javafx/controllers/GameController.java
@@ -1,20 +1,25 @@
 package coingame.javafx.controllers;
 
 import javafx.application.Platform;
+import javafx.beans.binding.ObjectBinding;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import org.tinylog.Logger;
 
 public class GameController {
     /**
-     * display the name of the player on top
+     * d. display the name of the player on top
      * display the board on the left
      * display a timer and number of moves made on the right
      * reset button to reset the state of the game
-     * finish button to stop the game
+     *  finish button to stop the game
      * */
 
     @FXML
@@ -32,6 +37,27 @@ public class GameController {
         Platform.runLater( ()-> {
             message.setText("Good Luck, "+ name+"!");
         });
+
+        createBoard();
+    }
+
+    //creates the board
+    private void createBoard(){
+        for(int i=0 ; i<gameBoard.getRowCount() ; i++){
+            for(int j=0 ; j<gameBoard.getColumnCount() ; j++){
+                var square = createSquare(i,j);
+                gameBoard.add(square,j,i);
+            }
+        }
+    }
+
+    //creates Squares
+    private StackPane createSquare(int row, int col){
+        var square = new StackPane();
+        square.getStyleClass().add("square");
+       // var coin = new Circle(50);
+
+        return square;
     }
 
     public void setName(String name){
